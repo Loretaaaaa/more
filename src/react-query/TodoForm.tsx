092af82b6c10ad9397190a679f025ec40edd8918
Loreta <badalyan.loreta@gmail.com>
2024-03-08 23:06:02 +0400

    optimistic updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,25 +3,40 @@ import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "./hooks/useTodos";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
-      //APPROACH 1: INVALIDATING THE CACHE
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"],
-      // })
 
-      //APPROACH 2: UPDATING THE DATA IN THE CACHE
-      queryClient.setQueryData<Todo[]>(["todos"],
-        (todos) => [savedTodo, ...(todos || [])],
-      );
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
       if (ref.current) ref.current.value = "";
+
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo.id === newTodo.id ? savedTodo : todo))
+      );
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -49,9 +64,7 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button
-            disabled={addTodo.isLoading}
-            className="btn btn-primary">
+          <button disabled={addTodo.isLoading} className="btn btn-primary">
             {addTodo.isLoading ? "Adding..." : "Add"}
           </button>
         </div>
