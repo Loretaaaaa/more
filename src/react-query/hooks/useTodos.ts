4b600ea5d06749dc1459cf015dcc5eecaa81f4c9
Loreta <badalyan.loreta@gmail.com>
2024-03-09 00:04:02 +0400

    Creating a reusable HTTP service

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,23 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
-
-const apiClient = new APIClient<Todo>("/todos");
-
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import todoService, { Todo } from "../services/todoService";
 
 const useTodos = () => {
   return useQuery<Todo[], AxiosError>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000,
-    // initialData: [],
   });
 };
 
